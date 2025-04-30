af74dc00449fbe5143a70d8df72fcd7ea519c867
PinkAvocadoDev <andreadistefano98@gmail.com>
2025-04-30 16:33:02 +0200

    fix config

--- php/inc/conf.php
+++ php/inc/conf.php
@@ -1,4 +1,3 @@
-<?php
-    $APIMASTERKEY= 'VGhpcyBhbmQgYWxsIGZ1dHVyZSByZXF1ZXN0cyBzaG91bGQgYmUgZGlyZWN0ZWQgdG8gdGhlIGdpdmVuIFVSSQ==';
+<?php;
     
     $REGIONARRAY = ["Abruzzo","Basilicata","Calabria","Campania","Emilia Romagna","Friuli Venezia Giulia","Lazio","Liguria","Marche","Molise","Puglia","Sardegna","Sicilia","Toscana","Veneto"];    
